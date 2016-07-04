79b713a183f9c208446ba3ca3f83509d74eb9f08
alw <747620155@qq.com>
2016-07-04 23:18:00 +0800

    knn分类 正确率55% 特征数据有待改善

--- FaceVerification.py
+++ FaceVerification.py
@@ -1,59 +1,234 @@
 # -*- coding: utf-8 -*-
 from facepp import API
 from facepp import File
+from numpy import *
+import string
+import operator
 import os
 import time
 
+
 dataBasePath = 'E:\\code\\Python\\FaceVerification\\data\\'
 groupName = 'alwTestalw'
+trainSetPath = dataBasePath+'trainSet.txt'
+trainLabelPath = dataBasePath+'trainLabel.txt'
+
+result_dict = {'Male':11,'Female':61,'Asian':11,'White':41,'Black':71,'None':11,'Dark':41,'Normal':71}
+
 
 
 '''
 描述：是否需要训练
 参数：[out]True,需要训练，False，不需要训练
 '''
-def isNeedTrain(facepp):
-    flag = True
-    groupDict = facepp.info.get_group_list()
-    for group in groupDict['group']:
-        if group['group_name'] == groupName:
-            flag = False
-            break
-    if flag:
-        facepp.group.create(group_name = groupName)
-        return True
-    return False
+def isNeedTrain():
+    if os.path.exists(trainSetPath) == True and os.path.exists(trainLabelPath) == True:
+        return False
+    return True
+
+
+
+def createTrainVec(detectResult1,detectResult2):
+    vec = []
+    if len(detectResult1['face']) > 0 and len(detectResult2['face']) > 0:
+        vec.append(detectResult1['face'][0]['attribute']['age']['range'])
+        vec.append(detectResult1['face'][0]['attribute']['age']['value'])
+        vec.append(result_dict[detectResult1['face'][0]['attribute']['gender']['value']])
+        vec.append(result_dict[detectResult1['face'][0]['attribute']['glass']['value']])
+        vec.append(detectResult1['face'][0]['attribute']['pose']['pitch_angle']['value'])
+        vec.append(detectResult1['face'][0]['attribute']['pose']['roll_angle']['value'])
+        vec.append(detectResult1['face'][0]['attribute']['pose']['yaw_angle']['value'])
+        vec.append(result_dict[detectResult1['face'][0]['attribute']['race']['value']])
+        vec.append(detectResult1['face'][0]['attribute']['smiling']['value'])
+        vec.append(detectResult2['face'][0]['attribute']['age']['range'])
+        vec.append(detectResult2['face'][0]['attribute']['age']['value'])
+        vec.append(result_dict[detectResult2['face'][0]['attribute']['gender']['value']])
+        vec.append(result_dict[detectResult2['face'][0]['attribute']['glass']['value']])
+        vec.append(detectResult2['face'][0]['attribute']['pose']['pitch_angle']['value'])
+        vec.append(detectResult2['face'][0]['attribute']['pose']['roll_angle']['value'])
+        vec.append(detectResult2['face'][0]['attribute']['pose']['yaw_angle']['value'])
+        vec.append(result_dict[detectResult2['face'][0]['attribute']['race']['value']])
+        vec.append(detectResult2['face'][0]['attribute']['smiling']['value'])
+    if len(vec) > 0:
+        return vec
+    return None
+
+'''
+将数据集中的特征值归一化
+'''
+def autoNormalize(dataSet):
+    minVal = dataSet.min(0)                     #最小的列值
+    maxVal = dataSet.max(0)                     #最大的列值
+    ranges = maxVal - minVal
+    normalDataSet = zeros(shape(dataSet))
+    m = dataSet.shape[0]
+    normalDataSet = dataSet - tile(minVal,(m,1))
+    normalDataSet = normalDataSet/tile(ranges,(m,1))
+    return normalDataSet
+
+'''
+描述：dump模型文件
+'''
+def dumpTrainFile(trainSet):
+    fl=open(trainSetPath, 'w')
+    for line in trainSet:
+        for value in line:
+            fl.write(str(value))
+            fl.write(',')
+        fl.write('\n')
+    fl.close()
+
+'''
+描述：dump标签文件
+'''
+def dumpLabelFile(trainLabel):
+    fl=open(trainLabelPath, 'w')
+    for label in trainLabel:
+        fl.write(str(label))
+        fl.write(',')
+    fl.close()
+
+'''
+描述：读取模型文件
+'''
+def loadTrainFile():
+    trainSet = []
+    fl = open(trainSetPath, 'r')
+    lines = fl.readlines()
+    for line in lines:
+        line.strip('\n')
+        line.strip(',')
+        tl = line.split(',')
+        if tl[len(tl)-1] == '\n':
+            tl = tl[:len(tl)-1]
+        tl = [float(e) for e in tl]
+        trainSet.append(tl)
+    fl.close()
+    return array(trainSet)
+
+'''
+描述：读取标签文件
+'''
+def loadLabelFile():
+    trainLabel = []
+    fl = open(trainLabelPath, 'r')
+    lines = fl.readline()
+    lines.strip('\n')
+    lines.strip(',')
+    tl = lines.split(',')
+    if tl[len(tl)-1] == '':
+        tl = tl[:len(tl)-1]
+    tl = [int(e) for e in tl]
+    trainLabel = tl
+    fl.close()
+    return trainLabel
 
 '''
 描述：训练数据集
 参数：[in]trainDataPath,str,训练数据集的路径
 '''
-def train(trainDataPath):
-    personCount = 0
+def train(facepp,trainDataPath):
+    trainSet = []
+    trainLabel = []
+    tmpCount = 0
+    #获得人名与图片路径字典
+
+    personDict = {}
     for parent,dirnames,filenames in os.walk(trainDataPath):    #三个参数：分别返回1.父目录 2.所有文件夹名字（不含路径） 3.所有文件名字
         for dirname in dirnames:
-            personCount+=1
-            if personCount > 100:
-                break
-            faceList = []
+            pathList = []
             for parent2,dirnames2,filenames2 in os.walk(trainDataPath+'\\'+dirname):
                 for filename2 in filenames2:
-                    result = facepp.detection.detect(img=File(trainDataPath+'\\'+dirname+'\\'+filename2), mode = 'oneface')
-                    if len(result['face']) > 0:
-                        faceList.append(result['face'][0]['face_id'])
-            if len(faceList)>0:
-                facepp.person.create(face_id = faceList,group_name = groupName,person_name = dirname)
-    session_id = facepp.train.identify(group_name = groupName)
-    facepp.wait_async(session_id['session_id'])
+                    pathList.append(trainDataPath+'\\'+dirname+'\\'+filename2)
+            personDict[dirname] = pathList
+
+    print personDict
+
+    #生成正样本和标签集
+    for personList in personDict.values():
+        if len(personList) > 1:
+            i = 0
+            while i <= len(personList)-1:
+                j = i+1
+                while j <= len(personList)-1:
+                    result1 = facepp.detection.detect(img = File(personList[i]),attribute = ['gender', 'age', 'race', 'smiling', 'glass', 'pose'])
+                    result2 = facepp.detection.detect(img = File(personList[j]),attribute = ['gender', 'age', 'race', 'smiling', 'glass', 'pose'])
+                    vec = createTrainVec(result1,result2)
+                    if vec is not None:
+                        trainSet.append(vec)
+                        trainLabel.append('1')
+                    j+=1
+                    tmpCount = tmpCount+1
+                    print tmpCount
+                    if tmpCount%1000 == 0:
+                        print 'sleep 10s in postive'
+                        time.sleep(10)
+                i+=1
+
+    #生成负样本和标签集
+    personList = personDict.values()
+    personList = personList[:len(personList)/5]
+    i = 0
+    while i < len(personList):
+        j = i+1
+        while j < len(personList):
+            k = 0
+            while k < len(personList[i]):
+                p = 0
+                while p < len(personList[j]):
+                    result1 = facepp.detection.detect(img = File(personList[i][k]),attribute = ['gender', 'age', 'race', 'smiling', 'glass', 'pose'])
+                    result2 = facepp.detection.detect(img = File(personList[j][p]),attribute = ['gender', 'age', 'race', 'smiling', 'glass', 'pose'])
+                    vec = createTrainVec(result1,result2)
+                    if vec is not None:
+                        trainSet.append(vec)
+                        trainLabel.append('0')
+                    p+=1
+                    tmpCount+=1
+                    print tmpCount
+                    if tmpCount%1000 == 0:
+                        print 'sleep 10s in negtive'
+                        time.sleep(10)
+                k+=1
+            j+=1
+        i+=1
+    dumpTrainFile(trainSet)
+    dumpLabelFile(trainLabel)
+
+    return array(trainSet),array(trainLabel)
+
+'''
+kNN算法分类器
+inX:测试向量
+dataSet:数据向量（不包含标签）
+labels:对应
+'''
+def kNNClassifyer(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]              #数据集行数
+    #tile(inX,(dataSetSize,1))表示将一个inX行向量拓展成dataSetSize行的矩阵，并用inX填充
+    #diffMat为inX与各行dataSet之间特征的差值
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2                      #将diffMat中的元素算个平方
+    #sqDistances是行向量，sqDiffMat.sum(axis=1)表示把第i行sqDiffMat的值做加法放在sqDistances[i]的位置
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5                #开方得到距离
+    sortedDistIndicies = distances.argsort()    #升序排序并将索引返回给sortedDistIndicies
+    classCount = {}
+    #计算与inX最近的K个dataSet的标签并保存至classCount
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0)+1
+
+    #对classCount降序排序，票数最多的标签为预测结果
+    sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1), reverse=True)
+    return int(sortedClassCount[0][0])
 
 
 
 '''
-描述：初始化face++,默认创建test组
+描述：初始化face++
 参数：[in]API_KEY,str
       [in]API_SECRET,str
       [out]api,face++接口实例
-      [out]flag,,是否需要训练
 '''
 def initFacepp(API_KEY,API_SECRET):
     if API_KEY is None:
@@ -61,11 +236,7 @@ def initFacepp(API_KEY,API_SECRET):
     if API_SECRET is None:
         API_SECRET='m7ZqIyi6rMcANlPhsgVnKfT7suV6o3lU'
     api = API(API_KEY, API_SECRET)
-
-    flag = True
-    #创建组
-    flag = isNeedTrain(facepp = api)
-    return api,flag
+    return api
 
 '''
 描述：解析并读取数据集
@@ -100,29 +271,25 @@ def phraseDataList(dataListPath):
 
 count = 0   #正确次数
 if __name__=='__main__':
-    facepp,needTrain = initFacepp(None,None)  
-    if needTrain == True:
-        train(dataBasePath+'trainPicutre')
-    dataSet = phraseDataList(dataBasePath+'datalist.txt')
+    facepp = initFacepp(None,None)
+    trainSet = None
+    trainLabel = None
+    if isNeedTrain():
+        trainSet,trainLabel = train(trainDataPath=dataBasePath+'trainPictrue3',facepp = facepp)
+    else:
+        trainSet = loadTrainFile()
+        trainLabel = loadLabelFile()
+    dataSet = phraseDataList(dataBasePath+'datalist2.txt')
     for data in dataSet:
+        testResult = 0
+        result1 = facepp.detection.detect(img = File(data['pic1']),attribute = ['gender', 'age', 'race', 'smiling', 'glass', 'pose'])
+        result2 = facepp.detection.detect(img = File(data['pic2']),attribute = ['gender', 'age', 'race', 'smiling', 'glass', 'pose'])
+        vec = createTrainVec(result1,result2)
         start = time.clock()
-        result1 = facepp.recognition.identify(img = File(data['pic1']),group_name = groupName)
-        result2 = facepp.recognition.identify(img = File(data['pic2']),group_name = groupName)
-        flag = False
-        realResult = '0'
-        confidence = 0
-        if len(result1['face'])>0 and len(result2['face'])>0:
-            for personN1 in result1['face'][0]['candidate']:
-                for personN2 in result2['face'][0]['candidate']:
-                    if personN1['person_name'] == personN2['person_name']:
-                        confidence = personN1['confidence']+50.0
-                        flag = True
-                        break
+        if vec is not None:
+            testResult = kNNClassifyer(vec, trainSet, trainLabel, 5)
         end = time.clock()
-        if flag == True:
-            realResult = '1'
-        if realResult == data['label']:
+        if testResult == int(data['label']):
             count+=1
-        print 'tset result:%s fact result:%s confidence:%f time:%.03f' % (realResult,data['label'], confidence, end-start)
-        #print 'test result:'+realResult+' fact result:'+data['label']+' confidence:'+str(confidence)+' time:'+str(end-start)
-    print 'correct count:'+str(count)+' correct ratio:'+str(count/6000.0)
+        print 'test result:%d fact result:%s time:%.03f' % (testResult,data['label'], end-start)
+    print 'correct count:'+str(count)+' correct ratio:'+str(count/1000.0)
